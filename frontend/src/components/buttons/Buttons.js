6deeaf8f5d2931a3df7f6283555d9c3bd4737a68
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2020-09-29 21:46:39 -0300

    css

--- frontend/src/components/buttons/Buttons.js
+++ frontend/src/components/buttons/Buttons.js
@@ -4,24 +4,20 @@ import SearchIcon from '@material-ui/icons/Search';
 import GroupAddRoundedIcon from '@material-ui/icons/GroupAddRounded';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 
-const Buttons = ({setRommIdNull, showSearch }) => {
-    return (
-        <div className="flex flex-shrink p-2 bg-white mb-2 rounded-md shadow-lg">
-            <button className="" onClick={setRommIdNull}>
-                <HomeRoundedIcon className="icons" />
-            </button>
-            <button
-                className=""
-                type="input"
-                onClick={showSearch}
-            >
-                <SearchIcon className="" />
-            </button>
-            <button className="">
-                <GroupAddRoundedIcon className="" />
-            </button>
-        </div>
-    )
-}
+const Buttons = ({ setRommIdNull, showSearch }) => {
+  return (
+    <div className="flex flex-shrink p-2 bg-white mb-2 rounded-md shadow-lg">
+      <button className="focus:outline-none" onClick={setRommIdNull}>
+        <HomeRoundedIcon className="icons" />
+      </button>
+      <button className="focus:outline-none" type="input" onClick={showSearch}>
+        <SearchIcon className="" />
+      </button>
+      <button className="focus:outline-none">
+        <GroupAddRoundedIcon className="" />
+      </button>
+    </div>
+  );
+};
 
 export default Buttons;
